ef85a64a9b95b572bbd117b7c9f03029f61cd875
k0g0ff <88585012+k0g0ff@users.noreply.github.com>
2022-04-14 17:53:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('button').onclick = myClick;
+var ans = 0;
+
+function myClick() {
+let proton = document.querySelector('.proton').value;
+let neitron = document.querySelector('.neitron').value;
+let yadr = document.querySelector('.yadr').value;
+let a = (proton * 1.00728) + (neitron * 1.00866);
+let b = a - yadr;
+let c = b * 1.661;
+let d = c * 9;
+let e = Math.round(d * 100000000) / 100000000;
+alert("Ответ: " + e + " х 10^-11(кг)");
+}
